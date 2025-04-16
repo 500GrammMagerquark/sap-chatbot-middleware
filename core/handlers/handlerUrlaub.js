1a14bb333eb1f96e737d6ee1f57ce51f52a16177
Mattis <m.joachim2002@gmail.com>
2025-04-16 19:13:59 +0200

    Inhalt der intente Router und handlerUrlaub ergänzt

---
+++ core/handlers/handlerUrlaub.js
@@ -0,0 +1,13 @@
+// core/handlers/handlerUrlaub.js
+
+async function getKontingent(userId) {
+    console.log("📥 Anfrage für Kontingent von", userId);
+  
+    // Später: SAP-OData-Aufruf
+    return {
+      text: `Hallo! Du hast noch 12 Urlaubstage übrig.`
+    };
+  }
+  
+  module.exports = { getKontingent };
+  
